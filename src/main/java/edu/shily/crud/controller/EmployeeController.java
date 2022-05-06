165431436a444e65cb6e71c46d2865dbe478ff83
Shily <1240323810@qq.com>
2022-05-06 13:23:05 +0800

    使用ajax完成基本页面

--- src/main/java/edu/shily/crud/controller/EmployeeController.java
+++ src/main/java/edu/shily/crud/controller/EmployeeController.java
@@ -3,12 +3,14 @@ package edu.shily.crud.controller;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import edu.shily.crud.bean.Employee;
+import edu.shily.crud.bean.Msg;
 import edu.shily.crud.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.annotation.RequestScope;
 
 import java.util.List;
@@ -24,6 +26,16 @@ public class EmployeeController {
     EmployeeService employeeService;
 
     @RequestMapping("/emps")
+    @ResponseBody
+    public Msg getEmpsWithJson(@RequestParam(value = "pn",defaultValue = "1") Integer pn, Model model){
+        PageHelper.startPage(pn,5);
+        List<Employee> emps = employeeService.getAll();
+        PageInfo page = new PageInfo(emps,5);
+
+        return Msg.success().add("pageInfo",page);
+    }
+
+    //@RequestMapping("/emps")
     public String getEmps(@RequestParam(value = "pn",defaultValue = "1") Integer pn,
                           Model model){
 
